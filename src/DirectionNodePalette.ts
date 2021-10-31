7178c7f2973e1472bd94e6232dce593bbe291be7
Aaron Faanes <dafrito@gmail.com>
2021-10-31 16:24:56 -0500

    Re-add NodePalette

---
+++ src/DirectionNodePalette.ts
@@ -0,0 +1,10 @@
+import DirectionNode from "./DirectionNode";
+import NodePalette from "./NodePalette";
+
+export default class DirectionNodePalette extends NodePalette<DirectionNode> {
+    spawn(given?:any):DirectionNode {
+        return new DirectionNode();
+    }
+    replace(node:DirectionNode, given?:any):void {
+    }
+}
---
+++ src/NodePalette.ts
@@ -0,0 +1,6 @@
+import DirectionNode from './DirectionNode';
+
+export default abstract class NodePalette<T extends DirectionNode> {
+  abstract spawn(given?:any):T;
+  abstract replace(node:T, given?:any):void;
+}
