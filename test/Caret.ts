b6c12aa1f46a07d1b8da96440fa5b44035da2dee
Aaron Faanes <dafrito@gmail.com>
2021-12-04 12:54:32 -0600

    Add tests

---
+++ test/Caret.ts
@@ -0,0 +1,24 @@
+import {assert, expect} from "chai";
+import {
+  Direction,
+  DirectionNode,
+  readDirection,
+  DirectionCaret,
+  FORWARD,
+  BACKWARD,
+  UPWARD,
+  DOWNWARD,
+  INWARD,
+  PreferredAxis,
+  namePreferredAxis,
+} from "../dist/parsegraph-direction";
+
+describe("Caret", function () {
+  it("can use a ", () => {
+    const car = new DirectionCaret("a");
+    assert.equal("a", car.node().value());
+    car.spawnMove("f", "b");
+    assert.equal("b", car.node().value());
+  });
+});
+
---
+++ test/DirectionNode.ts
@@ -0,0 +1,29 @@
+import {assert, expect} from "chai";
+import {
+  Direction,
+  DirectionNode,
+  readDirection,
+  DirectionCaret,
+  FORWARD,
+  BACKWARD,
+  UPWARD,
+  DOWNWARD,
+  INWARD,
+  PreferredAxis,
+  namePreferredAxis,
+} from "../dist/parsegraph-direction";
+
+describe("DirectionNode", function () {
+  it("can be constructed without a Type param", () => {
+    new DirectionNode();
+  });
+
+  it("can be constructed with a string param", () => {
+    const n = new DirectionNode<string>("No time");
+    assert.equal("No time", n.value());
+    n.setValue("Hey");
+    assert.equal("Hey", n.value());
+  });
+
+});
+
