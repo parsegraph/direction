8f8b98cc702cf3b55b241ccd99fd45a8f8ca3289
Aaron Faanes <dafrito@gmail.com>
2023-02-12 20:09:57 -0800

    Fix test files, and add tests

---
+++ test/DirectionNode.spec.ts
@@ -0,0 +1,37 @@
+import { assert } from "chai";
+import Direction, { PreferredAxis, DirectionNode, DirectionCaret } from "../src/index";
+
+describe("DirectionNode", function () {
+  it("can be constructed without a Type param", () => {
+    new DirectionNode();
+  });
+
+  it("can be constructed with a string param", () => {
+    const n = new DirectionNode<string>();
+    assert.equal(null, n.value());
+    n.setValue("Hey");
+    assert.equal("Hey", n.value());
+  });
+
+  it("can be pulled", () => {
+    const bud = new DirectionNode().setId("bud");
+    bud.setLayoutPreference(PreferredAxis.VERTICAL);
+    bud.connectNode(Direction.FORWARD, new DirectionNode().setId("f"));
+    bud.connectNode(Direction.BACKWARD, new DirectionNode().setId("b"));
+    bud.connectNode(Direction.DOWNWARD, new DirectionNode().setId("d"));
+    bud.connectNode(Direction.UPWARD, new DirectionNode().setId("u"));
+    console.log(bud.siblings().dump().map(n=>n.id()).join(", "))
+    bud.setLayoutPreference(PreferredAxis.HORIZONTAL);
+  });
+
+  it("can be pulled the other way", () => {
+    const bud = new DirectionNode().setId("bud");
+    bud.setLayoutPreference(PreferredAxis.HORIZONTAL);
+    bud.connectNode(Direction.FORWARD, new DirectionNode().setId("f"));
+    bud.connectNode(Direction.BACKWARD, new DirectionNode().setId("b"));
+    bud.connectNode(Direction.DOWNWARD, new DirectionNode().setId("d"));
+    bud.connectNode(Direction.UPWARD, new DirectionNode().setId("u"));
+    console.log(bud.siblings().dump().map(n=>n.id()).join(", "))
+    bud.setLayoutPreference(PreferredAxis.VERTICAL);
+  });
+});
